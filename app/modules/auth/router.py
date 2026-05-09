ab74fb57c65a4379da3f33125bf14e9112d49fb5
Ernesto Armenta <ernesto_armenta_2000@hotmail.com>
2026-05-08 17:50:32 -0700

    feat: login user

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from app.core.database import get_db
-from app.core.security import hash_password, create_access_token
+from app.core.security import hash_password, create_access_token, verify_password
 from .schemas import LoginRequest, RegisterRequest, LoginResponse
 from .model import User
 from datetime import timedelta
@@ -49,6 +49,38 @@ async def register(data: RegisterRequest, db=Depends(get_db)):
         )
 
 
-@router.post("/login")
-async def login(credentials: LoginRequest, db=Depends(get_db)):
-    return {"message": "Login"}
+@router.post("/login", response_model=LoginResponse)
+async def login(credentials: LoginRequest, response: Response, db=Depends(get_db)):
+    try:
+        # 1. Verify email exists
+        user_exists = db.query(User).filter(User.email == credentials.email).first()
+        if not user_exists:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="This email is not registered"
+            )
+
+        # 2. Verify password
+        if not verify_password(credentials.password, user_exists.hashed_password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="The password is not correct, please verify it"
+            )
+
+        # 3. Create and save access token
+        access_token = create_access_token({"sub": str(user_exists.id)})
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True
+        )
+
+        # 4. Response
+        return LoginResponse(message="User Login Successfully")
+    except HTTPException:
+        raise
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while trying to login",
+        )
